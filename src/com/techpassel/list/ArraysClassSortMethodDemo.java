97f0c8350ed32975ac93b059de59bc71da21aac8
techpassel <techpasselorg@gmail.com>
2020-09-22 02:37:03 +0530

    some minor changes

--- src/com/techpassel/list/ArraysClassSortMethodDemo.java
+++ src/com/techpassel/list/ArraysClassSortMethodDemo.java
@@ -5,7 +5,6 @@ import java.util.Comparator;
 
 public class ArraysClassSortMethodDemo {
     public static void main(String[] args) {
-        ArraysClassSortMethodDemo aObj = new ArraysClassSortMethodDemo();
         int[] ai = {1,62,39,16,21};
         System.out.println("Primitive array before sorting : ");
         for (int i=0; i<ai.length; i++){
@@ -19,10 +18,10 @@ public class ArraysClassSortMethodDemo {
 
         String[] s = {"Aman","Geetika","Samir","Manish","Pawan"};
         System.out.println("Object Array before sorting");
-        aObj.displayArrayObjects(s);
+        displayArrayObjects(s);
         System.out.println("Object Array after sorting using default natural sorting order : ");
         Arrays.sort(s);
-        aObj.displayArrayObjects(s);
+        displayArrayObjects(s);
         Comparator c = (o1, o2) -> {
             String s1 = (String) o1;
             String s2 = (String) o2;
@@ -30,10 +29,10 @@ public class ArraysClassSortMethodDemo {
         };
         System.out.println("Object Array after sorting using customized sorting order : ");
         Arrays.sort(s,c);
-        aObj.displayArrayObjects(s);
+        displayArrayObjects(s);
     }
 
-    public void displayArrayObjects(String[] s){
+    public static void displayArrayObjects(String[] s){
         for (String s1: s){
             System.out.println(s1);
         }
